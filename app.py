bc71b9a8d15c94da325e94d7141ea49f1c828824
aishacurly <ms.aisha01@hotmail.com>
2026-05-19 12:48:19 +0100

    test CI/CD pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from my container! It works!'
+    return 'CI/CD is working! Deployed automatically!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
